20f8318d5b3246461abad5a10fcead13c1450e5e
saraHoeferer <sara.hoeferer@stud.fh-campuswien.ac.at>
2023-03-21 12:56:33 +0100

    Small improvements design wise

--- app/src/main/java/com/example/movieapp/MainActivity.kt
+++ app/src/main/java/com/example/movieapp/MainActivity.kt
@@ -16,17 +16,22 @@ import androidx.compose.material.icons.filled.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.drawBehind
+import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.modifier.modifierLocalConsumer
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import coil.compose.AsyncImage
 import coil.request.ImageRequest
+import com.example.movieapp.models.Movie
 import com.example.movieapp.ui.theme.MovieAppTheme
+import com.example.movieapp.models.getMovies
 
-data class Movies(val name: String, val director: String, val relaseYear: Int, val genre: String, val actors: String, val rating: Double, val plot: String, val imageUrl: String);
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -38,47 +43,9 @@ class MainActivity : ComponentActivity() {
                     //modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    var movies = listOf<Movies>(
-                        Movies("Avatar 2",
-                            "James Cameron",
-                            2023,
-                            "Action, Adventure, Fantasy",
-                            "Sam Worthington, Zoe Saldena, Sigoamey Weaver, Stephen Lang",
-                            7.9,
-                            "Jake Sully lives with his newfound family formed on the extrasolar moon Pandora. Once a familiar threat returns to finish what was previously started, Jake must work with Neytiri and the army of the Na'vi race to protect their home.",
-                            "https://images.wallpapersden.com/image/download/avatar-2-the-way-of-water-movie-poster_bWxsaWeUmZqaraWkpJRmbmdlrWZlbWU.jpg"
-                        ),
-                        Movies("Being John Malkovich",
-                            "Spike Jonze",
-                            1999,
-                            "Comedy, Drama, Fantasy",
-                            "John Cusack, Catherine Keener, Cameron Diaz, John Malkovich",
-                            7.8,
-                            "A puppeteer discovers a portal that leads literally into the head of movie star John Malkovich.",
-                            "https://m.media-amazon.com/images/W/IMAGERENDERING_521856-T1/images/I/51XivNSv1oL._AC_UY1000_.jpg"
-                        ),
-                        Movies("Clockwork Orange",
-                            "Stanley Kubrick",
-                            1971,
-                            "Crime, Sci-Fi",
-                            "Malcolm McDowell, Patrick Magee, Michael Bates, Warren Clarke",
-                            8.3,
-                            "In the future, a sadistic gang leader is imprisoned and volunteers for a conduct-aversion experiment, but it doesn't go as planned.",
-                            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTaqugT7C4ez-MfvfzzUdjPxxr1urvyHlWlxQ&usqp=CAU"
-                        ),
-                        Movies("The Cat in the Hat",
-                            "Bo Welch",
-                            2003,
-                            "Adventure, Comedy, Family",
-                            "Mike Myers, Spencer Bresiln, Dakota Fanning",
-                            4.0,
-                            "Two bored children have their lives turned upside down when a talking cat comes to visit them.",
-                            "https://static.wikia.nocookie.net/soundeffects/images/9/9c/The_Cat_in_the_Hat_2003_Poster.png/revision/latest/scale-to-width-down/485?cb=20151206185937"
-                        )
-                    )
                     Column() {
                         TopAppBar()
-                        MovieList(movies = movies)
+                        MovieList(movies = getMovies())
                     }
                 }
             }
@@ -136,7 +103,7 @@ fun TopAppBar(){
 }
 
 @Composable
-fun MovieRow(movie: Movies){
+fun MovieRow(movie: Movie){
     var arrow by remember {
         mutableStateOf(Icons.Default.KeyboardArrowUp)
     }
@@ -147,37 +114,48 @@ fun MovieRow(movie: Movies){
     Card(modifier = Modifier
         .fillMaxWidth()
         .padding(5.dp),
-        shape = RoundedCornerShape(corner = CornerSize(10.dp)),
+        shape = RoundedCornerShape(corner = CornerSize(15.dp)),
         elevation = 5.dp
     ) {
-       Column(Modifier
-           .padding(5.dp)
-       ) {
+       Column() {
             Box(modifier = Modifier
                 .height(150.dp)
             )
             {
-                AsyncImage(
-                    model = ImageRequest.Builder(LocalContext.current)
-                        .data(movie.imageUrl)
-                        .crossfade(true)
-                        .build(),
-                    contentDescription = movie.name,
-                    contentScale = ContentScale.Crop,
-                    modifier = Modifier.fillMaxWidth()
-                )
-                //Image(painter = painterResource(id = R.drawable.avatar), contentDescription = "Avatar2", contentScale = ContentScale.Crop)
-                Box(modifier = Modifier
-                    .fillMaxSize()
-                    .padding(10.dp),
-                    contentAlignment = Alignment.TopEnd
-                ) {
-                    Icon(imageVector = Icons.Default.FavoriteBorder, contentDescription = "Like", tint=MaterialTheme.colors.secondary)
-                }
+                 AsyncImage(
+                        model = ImageRequest.Builder(LocalContext.current)
+                            .data(movie.images[0])
+                            .crossfade(true)
+                            .build(),
+                        contentDescription = movie.title,
+                        contentScale = ContentScale.Crop,
+                        modifier = Modifier
+                            .fillMaxWidth()
+                    )
+                    //Image(painter = painterResource(id = R.drawable.avatar), contentDescription = "Avatar2", contentScale = ContentScale.Crop)
+                    Box(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .padding(10.dp),
+                        contentAlignment = Alignment.TopEnd
+                    ) {
+                        Icon(
+                            imageVector = Icons.Default.FavoriteBorder,
+                            contentDescription = "Like",
+                            tint = MaterialTheme.colors.secondary
+                        )
+                    }
             }
-           Row () {
+           Row (modifier = Modifier
+               .padding(horizontal = 8.dp, vertical = 3.dp)
+               .height(35.dp),
+               verticalAlignment = Alignment.CenterVertically
+           ) {
                Column() {
-                   Text(text = movie.name)
+                   Text(text = movie.title,
+                        fontWeight = FontWeight.Bold,
+                        fontSize = 17.sp
+                   )
                }
                Column(modifier = Modifier
                    .fillMaxWidth(),
@@ -201,16 +179,18 @@ fun MovieRow(movie: Movies){
            Row() {
                AnimatedVisibility(visible = editable) {
                    Column(modifier = Modifier
-                       .padding(vertical = 10.dp)) {
+                       .fillMaxWidth()
+                       .padding(horizontal = 8.dp, vertical = 15.dp)) {
                        Text(text = "Director: ${movie.director}")
-                       Text(text = "Released: ${movie.relaseYear}")
+                       Text(text = "Released: ${movie.year}")
                        Text(text = "Genre: ${movie.genre}")
                        Text(text = "Actors: ${movie.actors}")
                        Text(text = "Rating: ${movie.rating}")
+                       Divider(color = Color.LightGray, thickness = 2.dp, modifier = Modifier.padding(vertical = 10.dp))
                        Text(
                            text = "Plot: ${movie.plot}",
                            modifier = Modifier
-                               .padding(horizontal = 30.dp, vertical = 15.dp)
+                               .padding(horizontal = 20.dp)
                        )
                    }
                }
@@ -221,7 +201,7 @@ fun MovieRow(movie: Movies){
 }
 
 @Composable
-fun MovieList(movies: List<Movies>){
+fun MovieList(movies: List<Movie>){
     LazyColumn(){
         items(movies){ movie ->
             MovieRow(movie = movie)
@@ -233,17 +213,7 @@ fun MovieList(movies: List<Movies>){
 @Composable
 fun DefaultPreview() {
     MovieAppTheme {
-        var movies = listOf<Movies>(
-            Movies("Avatar 2",
-                "James Cameron",
-                2023,
-                "Action, Adventure, Fantasy",
-                "Sam Worthington, Zoe Saldena, Sigoamey Weaver, Stephen Lang",
-                7.9,
-                "Jake Sully lives with his newfound family formed on the extrasolar moon Pandora. Once a familiar threat returns to finish what was previously started, Jake must work with Neytiri and the army of the Na'vi race to protect their home.",
-                "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwallpapersden.com%2Favatar-2-the-way-of-water-movie-poster-wallpaper%2F1920x1080%2F&psig=AOvVaw2Wr11scEHqWhY44hezLLE0&ust=1679401323228000&source=images&cd=vfe&ved=0CBAQjRxqFwoTCPjvjM2_6v0CFQAAAAAdAAAAABAJ"
-            )
-        )
+        var movies = getMovies()
         Column() {
             TopAppBar()
             MovieList(movies = movies)
